3e33d8481bd1e2ccc03aee0bd6e6b3ed934b024e
Salvador Z <salvador.zendejas@gmail.com>
2023-06-14 17:28:50 -0500

    uTest common macros

---
+++ lib/uTest_common.h
@@ -0,0 +1,42 @@
+/**
+ * @file uTest_common.h
+ * @author Salvador Z
+ * @version 1.0
+ * @brief File for common Macros and datatypes for uTest Framework
+ *
+ */
+
+#ifndef UTEST_COMMON_H_
+#define UTEST_COMMON_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Includes
+
+#define SEND_OUT() fflush(stdout)
+
+#ifndef PASS
+  #define PASS (0U)
+#endif
+
+#ifndef DISABLE
+  #define DISABLE (0U)
+#endif
+
+#ifndef ENABLE
+  #define ENABLE (1U)
+#endif
+
+#if !defined(PASSED) && !defined(FAILED)
+  #define PASSED(x)            (PASS == (x))
+  #define FAILED(x)            (PASS != (x))
+  #define EXPECTED_VAL(tst, x) ((tst) == (x))
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UTEST_COMMON_H_ */
